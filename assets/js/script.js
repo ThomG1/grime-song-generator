b83a1a77ebe2cb0d15a724f5c618e615de33d15f
ThomG1 <thomas.gwyther@hotmail.co.uk>
2022-12-10 09:36:21 +0000

    Add initial playPause BTN

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,29 @@ keys.forEach((key) => {
   key.addEventListener("click", playSound);
 });
 window.addEventListener('keydown', playSound, false);
+
+
+
+var audio = document.getElementById('audio');
+var playPauseBTN = document.getElementById('playPauseBTN');
+var count = 0;
+
+function playPause(){
+	if(count == 0){
+		count = 1;
+		audio.play();
+		playPauseBTN.innerHTML = "Pause ⏸";
+	}else{
+		count = 0;
+		audio.pause();
+		playPauseBTN.innerHTML = "Play ►";
+	}
+
+}
+
+function stop(){
+	playPause()
+	audio.pause();
+	audio.currentTime = 0;
+	playPauseBTN.innerHTML = "Play ►";
+}
