26bc5ae22a382d4a4b2f491e27d094f3d234963c
ThomG1 <thomas.gwyther@hotmail.co.uk>
2022-12-28 11:14:40 +0000

    Add initial emailjs

---
+++ contact.js
@@ -0,0 +1,10 @@
+function sendMail () {
+    var params = {
+        name:document.getElementById("fullname").value,
+        email:document.getElementById("emailaddress").value,
+        message:document.getElementById("mainmessage").value,
+
+    };
+}
+
+const serviceID = "service_zcjlh8m"
