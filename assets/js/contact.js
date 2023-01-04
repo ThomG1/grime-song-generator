7c5a29f081743bad79c56fabd144602918a76ad2
ThomG1 <thomas.gwyther@hotmail.co.uk>
2023-01-04 13:21:59 +0000

    Add images to credits modal

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -26,3 +26,34 @@ emailjs.send(serviceID,templateID,params)
 .catch((err)=> console.log(err));
 return false;
 }
+
+
+//Form result
+
+function message(){
+    var Name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var msg = document.getElementById('mainmessage');
+    const success = document.getElementById('success');
+    const danger = document.getElementById('danger');
+
+    if(Name.value === '' || email.value === '' || msg.value === ''){
+        danger.style.display = 'block';
+    }
+    else{
+        setTimeout(() => {
+            Name.value = '';
+            email.value = '';
+            msg.value = '';
+        }, 2000);
+
+        success.style.display = 'block';
+    }
+
+
+    setTimeout(() => {
+        danger.style.display = 'none';
+        success.style.display = 'none';
+    }, 4000);
+
+}
